182dc900d2ed3f0dcc6f3df7cff9528fe66ed1a2
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2023-05-21 22:27:53 +0530

    Responsive mobile

--- src/components/Capsule.tsx
+++ src/components/Capsule.tsx
@@ -8,15 +8,18 @@ type TProps = {
 const Capsule = ({ text, type = "info", additionalStyles, icon }: TProps) => {
   let typeBg = "bg-slate-800/20";
   let typeColor = "text-slate-300";
+  let typeBorder = "border-slate-500/50";
 
   switch (type) {
     case "info":
       typeBg = "bg-blue-800/20";
       typeColor = "text-blue-300";
+      typeBorder = "border-blue-500/50";
       break;
     case "warning":
       typeBg = "bg-yellow-800/20";
       typeColor = "text-yellow-300";
+      typeBorder = "border-yellow-500/50";
       break;
     default:
       break;
@@ -24,7 +27,7 @@ const Capsule = ({ text, type = "info", additionalStyles, icon }: TProps) => {
 
   return (
     <div
-      className={`flex items-center justify-center group w-fit text-sm px-3 py-1.5 rounded-full ${typeBg} ${typeColor} ${additionalStyles}`}
+      className={`flex items-center justify-center group w-fit text-xs xl:text-sm px-3 py-1.5 rounded-full border ${typeBg} ${typeColor} ${typeBorder} ${additionalStyles}`}
     >
       {text} {icon}
     </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,36 +18,44 @@ export default function Home({
 
   return (
     <main
-      className={`flex min-h-screen flex-col py-8 bg-gradient-to-bl from-violet-950 via-zinc-950 to-black`}
+      className={`flex min-h-screen flex-col px-4 py-4 xl:py-8 bg-gradient-to-b xl:bg-gradient-to-bl from-violet-950 via-zinc-950 to-black`}
     >
-      <section className="w-3/4 flex flex-col mx-auto">
+      <section className="w-full xl:w-4/5 2xl:w-3/4 flex flex-col mx-auto">
         <div className="flex items-center justify-between">
-          <LogoLarge className="w-24 h-fit text-white" />
+          <LogoLarge className="w-20 xl:w-24 h-fit text-white" />
         </div>
-        <div className="flex items-center justify-between mt-20">
-          <div className="flex flex-col justify-start max-w-[640px]">
+        <div className="flex flex-col xl:flex-row items-center justify-between mt-8 xl:mt-20">
+          <div className="flex flex-col items-center xl:items-start justify-start w-full xl:max-w-[640px]">
             <Link href="/changelog">
               <Capsule
                 text={`v${data.version} is out! See what's new`}
-                additionalStyles="mb-4 hover:bg-blue-700/20 pointer"
+                additionalStyles="mb-2 xl:mb-4 hover:bg-blue-700/20 pointer"
                 icon={
                   <RightArrow className="w-4 h-4 text-blue-300 group-hover:text-blue-400 ml-2" />
                 }
               />
             </Link>
-            <h1 className="text-6xl py-2 tracking-tighter text-transparent font-bold text- bg-clip-text bg-gradient-to-br from-slate-50 to-violet-700">
+            <h1 className="text-5xl xl:text-6xl py-2 tracking-tighter text-transparent font-bold text- bg-clip-text bg-gradient-to-br from-slate-50 to-violet-700">
               Get going faster.
             </h1>
-            <p className="mt-4 text-xl text-slate-300">
+            <p className="mt-2 mx-1 xl:mx-0 xl:mt-4 text-base xl:text-xl text-slate-300">
               Yack is a MacOS app that lets you access ChatGPT right at your
-              fingertips. Your favourite AI assistant now lives in your{" "}
+              fingertips.{" "}
+            </p>
+            <p className="mt-2 mx-1 xl:mx-0 xl:mt-4 text-base xl:text-xl text-slate-300">
+              Your favourite AI assistant now lives in your{" "}
               <strong className="border-b border-dotted border-slate-500 pb-0.5">
                 menu bar
               </strong>
               .
             </p>
           </div>
-          <video className="w-2/5 rounded-lg" autoPlay loop muted>
+          <video
+            className="w-full mt-8 xl:mt-0 xl:w-2/5 rounded-lg"
+            autoPlay
+            loop
+            muted
+          >
             <source src="/yack_demo.mp4" type="video/mp4" />
           </video>
         </div>
