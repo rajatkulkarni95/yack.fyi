2387e0c0a7d31db0d5a6037b4143131ea88863e9
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2023-06-01 16:52:41 +0530

    Fix typo, add search feature

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -3,7 +3,7 @@
 // Colors should be some variation of the following:
 // red, green, blue, yellow, purple, pink, orange, teal, indigo, gray, coolGray, trueGray, warmGray, cyan, fuchsia, rose
 
-import { TFeature } from "~/data/features";
+import { TFeature } from '~/data/features'
 
 const FeatureCard = ({ feature }: { feature: TFeature }) => {
   return (
@@ -12,15 +12,15 @@ const FeatureCard = ({ feature }: { feature: TFeature }) => {
         <p className="text-violet-300 font-bold text-base">{feature.title}</p>
         {feature.comingSoon && (
           <span className="text-violet-400 bg-violet-700/30 border-violet-600/50 border rounded uppercase text-[9px] px-2 py-1">
-            Coming Soon{" "}
+            Coming Soon{' '}
           </span>
         )}
       </div>
-      <p className="text-zinc-200 font-normal mt-4 text-sm">
+      <p className="text-zinc-200 font-normal !mt-4 text-sm">
         {feature.description}
       </p>
     </div>
-  );
-};
+  )
+}
 
-export default FeatureCard;
+export default FeatureCard
--- src/data/features.ts
+++ src/data/features.ts
@@ -1,65 +1,72 @@
 export type TFeature = {
-  id: number;
-  title: string;
-  description: string;
-  comingSoon: boolean;
-};
+  id: number
+  title: string
+  description: string
+  comingSoon: boolean
+}
 
 export const features: TFeature[] = [
   {
     id: 1,
-    title: "Fast and Lightweight",
+    title: 'Fast and Lightweight',
     description:
-      "Built with Rust, Yack weighs in at < 10 MB, none of that 250MB Electron app nonsense.",
+      'Built with Rust, Yack weighs in at < 10 MB, none of that 250MB Electron app nonsense.',
     comingSoon: false,
   },
   {
     id: 2,
-    title: "Private",
+    title: 'Private',
     description:
       "We don't store any of your data on our servers. Anything to be stored, is stored on your device, and never leaves it.",
     comingSoon: false,
   },
   {
     id: 3,
-    title: "Multiple Themes",
+    title: 'Multiple Themes',
     description:
-      "Light, Dark, and colorful! Choose from a variety of themes to suit your mood.",
+      'Light, Dark, and colorful! Choose from a variety of themes to suit your mood.',
     comingSoon: false,
   },
   {
     id: 4,
-    title: "Keyboard-first Design",
+    title: 'Keyboard-first Design',
     description:
       "Is it really 'fast' if you can't use it without a mouse? Yack is designed to be used with a keyboard, so you get going faster.",
     comingSoon: false,
   },
   {
     id: 5,
-    title: "Markdown Support",
+    title: 'Markdown Support',
     description:
-      "Bullets, Numbered Lists, Codeblocks, Tables. You name it, we parse it.",
+      'Bullets, Numbered Lists, Codeblocks, Tables. You name it, we parse it.',
     comingSoon: false,
   },
   {
     id: 6,
-    title: "Cross App Integration",
+    title: 'Cross App Integration',
     description:
-      "Won't it be cool if you Yack could make it easier to pick context from other apps? We think so too.",
+      "Won't it be cool if Yack could pick context from other apps? We think so too.",
     comingSoon: true,
   },
   {
     id: 7,
-    title: "Prompt Templates",
+    title: 'Prompt Templates',
     description:
       "Act as ______. Translate ______ to _______. We get it, you have your own prompts. We'll let you save them.",
     comingSoon: true,
   },
   {
     id: 8,
-    title: "Prompt Completion",
+    title: 'Prompt Completion',
     description:
       "You know how Google suggests the rest of your sentence? We'll do that, but for your prompts.",
     comingSoon: true,
   },
-];
+  {
+    id: 9,
+    title: 'Search',
+    description:
+      "Look, we know you're going to have a lot of chats. We'll let you search through them.",
+    comingSoon: true,
+  },
+]
