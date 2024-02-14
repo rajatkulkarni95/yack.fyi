0b0cd5f4fe9ab9c4de96cc0960e1533ea246df8a
Rajat Kulkarni <rajatkulkarni95@gmail.com>
2024-02-14 20:05:30 +0530

    Remove pricing page, add open source capsule

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -28,11 +28,6 @@ export default function Home({
             <LogoLarge className="w-20 xl:w-24 text-white" />
           </Link>
           <div className="flex items-center justify-between">
-            <Link href="/pricing">
-              <div className="text-sm text-slate-300 hover:text-slate-50 px-4 py-2 bg-transparent rounded-md hover:bg-violet-300/20 cursor-pointer">
-                Pricing
-              </div>
-            </Link>
             <Link href="/changelog">
               <div className="text-sm text-slate-300 hover:text-slate-50 px-4 py-2 bg-transparent rounded-md hover:bg-violet-300/20 cursor-pointer">
                 Changelog
@@ -42,15 +37,19 @@ export default function Home({
         </div>
         <div className="flex flex-col xl:flex-row items-center justify-between mt-8 xl:mt-20">
           <div className="flex flex-col items-center xl:items-start justify-start w-full xl:max-w-[640px]">
-            <Link href="/changelog">
+            <a
+              href="https://github.com/rajatkulkarni95/yack"
+              target="_blank"
+              rel="noreferrer noopener"
+            >
               <Capsule
-                text={`v${data.version} is out! See what's new`}
+                text={`Yack is now Open Source!`}
                 additionalStyles="mb-2 xl:mb-4 hover:bg-violet-700/20 pointer"
                 icon={
                   <RightArrow className="w-4 h-4 text-violet-300 group-hover:text-violet-400 ml-1" />
                 }
               />
-            </Link>
+            </a>
             <h1 className="text-4xl md:text-6xl py-2 tracking-tighter text-transparent font-bold bg-clip-text bg-gradient-to-br from-slate-50 to-violet-700">
               Get going faster.
             </h1>
